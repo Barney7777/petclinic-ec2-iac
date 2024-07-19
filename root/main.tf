462cec1186ad16b3c53bb5d3ead67b7a3a3984f2
Barney Wang <wangyaxu7@gmail.com>
2024-07-19 12:38:43 +1000

    add tf files

--- root/main.tf
+++ root/main.tf
@@ -1,5 +1,33 @@
 locals {
   project_name = var.project_name
   environment  = var.environment
+  region       = var.region
+}
+
+# Create vpc
+module "vpc" {
+  source                       = "../modules/vpc"
+  project_name                 = local.project_name
+  environment                  = local.environment
+  vpc_cidr                     = var.vpc_cidr
+  public_subnet_az1_cidr       = var.public_subnet_az1_cidr
+  public_subnet_az2_cidr       = var.public_subnet_az2_cidr
+  private_app_subnet_az1_cidr  = var.private_app_subnet_az1_cidr
+  private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
+  private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
+  private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
+}
+
+module "nat-gateway" {
+  source                    = "../modules/nat-gateway"
+  project_name              = local.project_name
+  environment               = local.environment
+  public_subnet_az1_id      = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id      = module.vpc.public_subnet_az2_id
+  vpc_id                    = module.vpc.vpc_id
+  private_app_subnet_az1_id = module.vpc.private_app_subnet_az1_id
+  private_app_subnet_az2_id = module.vpc.private_app_subnet_az2_id
+  igw_id                    = module.vpc.igw_id
+
 }
 
--- root/variables.tf
+++ root/variables.tf
@@ -3,4 +3,11 @@ variable "region" {}
 variable "project_name" {}
 variable "environment" {}
 
-
+# VPC variables
+variable "vpc_cidr" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+variable "private_app_subnet_az1_cidr" {}
+variable "private_app_subnet_az2_cidr" {}
+variable "private_data_subnet_az1_cidr" {}
+variable "private_data_subnet_az2_cidr" {}
